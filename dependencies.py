18cf9135f179dadad8e336dec3a27eb622e4b219
René Fritze <rene.fritze@wwu.de>
2020-06-30 15:19:16 +0200

    [docs|setup] add bash_kernel as dependencies for docs

--- dependencies.py
+++ dependencies.py
@@ -47,7 +47,7 @@ install_suggests = {'ipython>=5.0': 'an enhanced interactive python shell',
 doc_requires = ['sphinx>=1.7', 'jupyter_sphinx', 'matplotlib', 'PySide2', 'ipyparallel>=6.2.5',
                 'ipywidgets', 'sphinx-qt-documentation'] + install_requires
 ci_requires = [_PYTEST, 'pytest-cov', 'pytest-xdist', 'check-manifest', 'nbconvert', 'pytest-parallel', 'virtualenv',
-               'readme_renderer[md]', 'rstcheck', 'codecov', 'twine', 'pytest-memprof', 'pytest-timeout',
+               'readme_renderer[md]', 'rstcheck', 'codecov', 'twine', 'pytest-memprof', 'pytest-timeout', 'bash_kernel',
                'testipynb', "pypi-oldest-requirements>=2020.2", 'hypothesis[numpy,pytest]', ]
 import_names = {'ipython': 'IPython',
                 'pytest-cache': 'pytest_cache',
